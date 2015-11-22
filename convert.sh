0cb1e64f466546663db6f16dcfd6cf8b22abcfb1
Andres Kütt <andres.kytt@gmail.com>
2015-11-22 17:34:37 +0200

    Fixed a bug generating invalid labels

--- convert.sh
+++ convert.sh
@@ -1,2 +1,2 @@
-find ./content/ -type f -name '*.md' -print | while read filename; do echo "\label{" $filename "}" | sed 's,.md,,' | sed -e 's/[ ./]//g'; cat $filename; printf "\n\n"; done | ./md2latex.py
+find ./content/ -type f -name '*.md' -print | while read filename; do echo "\label{section:" $filename "}" | sed 's,.md,,' | sed -e 's/[ ./]//g' | sed 's,content,,'; cat $filename; printf "\n\n"; done | ./md2latex.py
 
